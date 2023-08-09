ea990b90d5edffb0205dbe9ffed7daec1c1e5ad3
suneiang <suneiang@suneiangs-MacBook-Air.local>
2023-08-09 20:21:06 +0700

    My initial dotfiles.

---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,303 @@
+-- Install packer
+local ensure_packer = function ()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Initialize packer
+require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+ display = {
+   open_fn = function()
+     return require('packer.util').float({ border = 'solid' })
+   end,
+ },
+})
+
+-- Install plugins
+local use = require('packer').use
+
+use('wbthomason/packer.nvim') -- Let packer manage itself
+
+use({
+  'jessarcher/onedark.nvim',
+  config = function()
+    vim.cmd('colorscheme onedark')
+
+    vim.api.nvim_set_hl(0, 'FloatBorder', {
+        fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+        bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+      })
+    -- Move the cursor line background invisible
+    vim.api.nvim_set_hl(0, 'CursorLineBg', {
+        fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+        bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+      })
+    
+    vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+
+    vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+        fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+        bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+      })
+
+    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
+    end,
+})
+
+-- Plugins
+use('christoomey/vim-tmux-navigator')
+use('farmergreg/vim-lastplace')
+use('tpope/vim-commentary')
+use('tpope/vim-repeat')
+use('tpope/vim-surround')
+use('tpope/vim-eunuch') -- Adds :Rename, :SudoWrite
+use('tpope/vim-unimpaired') -- Adds [b and other handy mappings
+use('tpope/vim-sleuth') -- Indent autodetection with editorconfig support
+use('sheerun/vim-polyglot') -- Add more languages.
+use('jessarcher/vim-heritage') -- Automatically create parent dirs when saving
+use('nelstrom/vim-visual-star-search')
+
+use({
+  'whatyouhide/vim-textobj-xmlattr',
+  requires = 'kana/vim-textobj-user',
+})
+
+use({
+  'airblade/vim-rooter',
+  setup = function()
+    vim.g.rooter_manual_only = 1
+  end,
+  config = function()
+    vim.cmd('Rooter')
+  end,
+})
+
+use({
+  'windwp/nvim-autopairs',
+  config = function()
+    require('nvim-autopairs').setup()
+  end,
+})
+
+use({
+  'karb94/neoscroll.nvim',
+  config = function()
+    -- require('user.plugins.neoscroll')
+    require('neoscroll').setup()
+  end,
+})
+
+use({
+  'famiu/bufdelete.nvim',
+  config = function()
+    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+  end,
+})
+
+use({
+  'AndrewRadev/splitjoin.vim',
+  config = function()
+      vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+      vim.g.splitjoin_trailing_comma = 1
+      vim.g.splitjoin_php_method_chain_full = 1
+    -- require('user.plugins.splitjoin')
+  end,
+})
+
+use({
+  'sickill/vim-pasta',
+  config = function()
+      vim.g.pasta_disabled_filetypes = { 'fugitive' }
+    -- require('user.plugins.pasta')
+  end,
+})
+
+use({
+  'phpactor/phpactor',
+  ft = 'php',
+  run = 'composer install --no-dev -o',
+  config = function()
+    vim.keymap.set('n', '<leader>pm', ':PhpactorContextMenu<CR>')
+    vim.keymap.set('n', '<leader>pn', ':PhpactorClassNew<CR>')
+  end,
+})
+
+use({
+  'tpope/vim-projectionist',
+  requires = 'tpope/vim-dispatch',
+  config = function()
+    require('user.plugins.projectionist')
+  end,
+})
+
+-- Fuzzy finder
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    { 'nvim-telescope/telescope-live-grep-args.nvim' },
+  },
+  config = function()
+    require('user.plugins.telescope')
+  end,
+})
+
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user.plugins.nvim-tree')
+  end,
+})
+
+use({
+  'nvim-lualine/lualine.nvim',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user.plugins.lualine')
+  end,
+})
+
+use({
+  'akinsho/bufferline.nvim',
+  requires = 'kyazdani42/nvim-web-devicons',
+  after = 'onedark.nvim',
+  config = function()
+    require('user.plugins.bufferline')
+  end,
+})
+
+use({
+  'lukas-reineke/indent-blankline.nvim',
+  config = function()
+    require('user.plugins.indent-blankline')
+  end,
+})
+
+use({
+  'lewis6991/gitsigns.nvim',
+  requires = 'nvim-lua/plenary.nvim',
+  config = function()
+    require('gitsigns').setup()
+      -- sign_priority = 20,
+      -- on_attach = function(bufnr)
+        vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+        vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+        vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+        vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+        vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+        vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+        -- vim.keymap.set('n', ']h', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true, buffer = bufnr })
+        -- vim.keymap.set('n', '[h', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true, buffer = bufnr })
+      -- end,
+    -- })
+  end,
+})
+
+use({
+  'tpope/vim-fugitive',
+  requires = 'tpope/vim-rhubarb',
+})
+
+-- Floating terminal.
+use({
+  'voldikss/vim-floaterm',
+  config = function()
+    vim.g.floaterm_width = 0.8
+    vim.g.floaterm_height = 0.8
+    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+    -- require('user.plugins.floaterm')
+
+    vim.cmd([[
+      highlight link Floaterm CursorLine
+      highlight link FLoatermBorder CursorLineBg
+      ]])
+  end,
+})
+
+
+use({
+  'nvim-treesitter/nvim-treesitter',
+  run = function()
+    require('nvim-treesitter.install').update({ with_sync = true })
+  end,
+  -- run = ':TSUpdate',
+  requires = {
+  --   'nvim-treesitter/playground',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  config = function()
+    require('user.plugins.treesitter')
+  end,
+})
+
+-- Language Server Protocol.
+use({
+  'neovim/nvim-lspconfig',
+  requires = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'b0o/schemastore.nvim',
+  },
+  config = function()
+    require('user.plugins.lspconfig')
+  end,
+})
+
+use({
+  'jose-elias-alvarez/null-ls.nvim',
+  config = function()
+    require('user.plugins.null-ls')
+  end,
+})
+
+use({
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-buffer',
+    -- 'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    -- 'hrsh7th/cmp-nvim-lua',
+    'jessarcher/cmp-path',
+    'onsails/lspkind-nvim',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    require('user.plugins.cmp')
+  end,
+})
+
+use({
+  'vim-test/vim-test',
+  config = function()
+    require('user.plugins.vim-test')
+  end,
+})
+
+-- Automatically install plugins on first run
+if packer_bootstrap then
+  require('packer').sync()
+end
+
+-- Automatically regenerate compiled loader file on save
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile>
+  augroup end
+]])
---
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -0,0 +1,59 @@
+require('mason').setup()
+require('mason-lspconfig').setup({ automatic_installation = true })
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require('lspconfig').intelephense.setup({
+    capabilities = capabilities
+})
+
+require'lspconfig'.phpactor.setup{
+  -- cmd = { '/home/suneiang/.local/share/nvim/site/pack/packer/opt/phpactor/bin/phpactor', 'language-server' },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  -- init_options = {
+  --   ["language_server_phpstan.enabled"] = false,
+  --   ["language_server_psalm.enabled"] = false,
+  -- }
+}
+
+require'lspconfig'.volar.setup{
+  capabilities = capabilities,
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+}
+require('lspconfig').tailwindcss.setup({
+    capabilities = capabilities
+})
+
+require('lspconfig').jsonls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    json = {
+      schemas = require('schemastore').json.schemas(),
+    },
+  },
+})
+
+vim.keymap.set('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
+vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+
+-- Diagnostic configuration
+vim.diagnostic.config({
+    virutal_text = false,
+    float = {
+      source = false
+    }
+})
+
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
