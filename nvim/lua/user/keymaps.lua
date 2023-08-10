0fa204e7449d70118285751c50db3a5441f22833
suneiang <suneiang@cashier-qa.genkisushi.com.kh>
2023-08-10 09:08:05 +0700

    add nvim window management keymaps.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -30,6 +30,12 @@ vim.keymap.set('i', ',,', '<Esc>A,<Esc>')
 -- Open the current file in the default program (on Mac this should just be just `open`)
 vim.keymap.set('n', '<leader>x', ':!open %<cr><cr>')
 
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
 -- Disable annoying command line thing
 vim.keymap.set('n', 'q:', ':q<CR>')
 
